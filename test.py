0ad288dc61f7a6383115acaef672dfc3ff14f9ed
rochungnungahrangchal <nunga.h@nic.in>
2022-08-19 17:02:31 -0700

    Commented

--- test.py
+++ test.py
@@ -10,20 +10,26 @@ from prophet.plot import plot_plotly, plot_components_plotly
 df = pd.read_csv('datasets.csv')
 
 m = Prophet()
+""" Kan rama Holidays te a lo telh theih vek a nih chu!!!... mahse mawww.... Diwali leh holi chiah an telh niaa.. chu pawh  from 2010 to 2030!!!!...whmmmmppppps"""
 
 m.add_country_holidays(country_name='IN')
 
-"""m = Prophet()"""
+""" Hemi Block hi kan dataset a felfai chuan a ngai lo """
 
 df2=df.copy()
 df2['ds'] = pd.to_datetime(df2['ds'])
+""" zing dar 7 hmalam data chu kan duh lo"""
 
 df2 = df2[df2['ds'].dt.hour > 7 ]
 
-print(df2.head(15))
+df3=df2.copy()
+df3['ds'] = pd.to_datetime(df3['ds'])
+""" Tlai dar 6 hmalam data chu kan duh lo"""
+df3 = df3[df3['ds'].dt.hour < 19 ]
+""" Block Tawpna """
 
-m.fit(df2)
-df.plot()
+""" Kan dataframe kha Prophet-ah kan fit anf= kan barh ang"""
+m.fit(df3)
 
 """ Hei hi a dik reng """
 
@@ -31,31 +37,37 @@ future = m.make_future_dataframe(periods=22,freq="H")
 
 future2 = future.copy()
 
+""" KAn Future data tur hian zing dar 7 hmalam data chu kan duh lo"""
 future2 = future2[future2['ds'].dt.hour > 7]
-
 future3=future2.copy()
 
+""" KAn Future data tur hian Tlai dar 6 hnulam data chu kan duh loooooooo"""
 future3 = future3[future3['ds'].dt.hour < 19]
 
+""" Kan Future data(frame) remdik hmangin kan predict ang!!!!!"""
 forecast = m.predict(future3)
 
 """ Kan Forecast data hi Json file-ah kan save ang"""
+""" a Key kan dah luh tur kan filter ang... array?(python hian array index-ah integer ni lo hman a phal ve tlatsss... ha haaa) indexing...."""
 
 jsondata=forecast[["yhat","yhat_lower"]].to_json() 
-""" a Key kan dah luh tur kan filter ang"""
+
+""" Json file kan siam ang!!!.. WRITE-access nei turin!!!!!! """
 
 with open("forecast.json", "w") as outfile:
     outfile.write(jsondata)
 
-"""print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(22))"""
+""" Forecast Data En ve chhin hrim hrim ang!!!"""
 
 print(forecast.tail(5))
 
 fig1 = m.plot(forecast)
 
 fig2 = m.plot_components(forecast)
+
 plot_plotly(m, forecast)
 
 plot_components_plotly(m, forecast) 
 
+""" Kan Graph a lan theih nan!!!!... Anaconda Spyder ka hmanin hei hi a ngai bawk si lo aaaa..min ti buai latukkkkk!!!! """
 pyplot.show()
